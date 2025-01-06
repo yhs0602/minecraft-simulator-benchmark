d508249a87a71627c68a72c8ccb20fd679589243
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-06 14:42:31 +0900

    :pencil: Wirte docs about minerl install: thanks to https://github.com/MineDojo/MineDojo/issues/113

--- README.md
+++ README.md
@@ -25,29 +25,75 @@
 - load: simulation, render, ppo, render_ppo, optimized_render, optimized_ppo, optimized_render_ppo
 
 # Environment Setup
-- Conda
-- MULS, single process at once
-- APPLE, single process at once, normal load (not strict setting)
+- Conda, python=3.11
+- Ubuntu Ubuntu 18.04.6 LTS, cuda driver version 525.105.17, NVIDIA GeForce RTX 3090 Ti, VirtualGL, RAM 188GB, AMD Ryzen Threadripper 3960X 24-Core Processor
+- Apple M1 Pro, single process at once,  single process at once, normal load (not strict setting)
 
-### MineRL 0.4.4 (minerl044)
+### MineRL 0.4.4 (exp_minerl044)
 ```bash
-conda install conda-forge::openjdk=8 
-pip install setuptools==65.5.0 pip==21
-pip install wheel==0.38.0
-pip install minerl==0.4.4
-
+conda create -n exp_minerl044 python=3.11
 conda activate exp_minerl044
+conda install conda-forge::openjdk=8 
+pip install setuptools==65.5.0 pip==21 wheel==0.38.0
+pip install minerl==0.4.4 # error because of MixinGradle
+pip download --no-binary :all: minerl==0.4.4 # Hangs in installing build dependencies
+pip install --upgrade pip
+pip download --no-binary :all: minerl==0.4.4 # Does not hang? openssl error
+OPENSSL_ROOT_DIR=$CONDA_PREFIX pip download --no-binary :all: minerl==0.4.4
+# Takes a long long time to install
+# ERROR: Problem encountered: NumPy requires GCC >= 8.4
+sudo apt update
+sudo apt install -y software-properties-common
+sudo apt-get install gcc-8 g++-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 100
+OPENSSL_ROOT_DIR=$CONDA_PREFIX pip download --no-binary :all: minerl==0.4.4
+sudo apt install -y libxml2-dev libxslt1-dev
+OPENSSL_ROOT_DIR=$CONDA_PREFIX pip download --no-binary :all: minerl==0.4.4
+# ERROR: Problem encountered: SciPy requires GCC >= 9.1
+# Use this table to get latest gcc version your OS supports: https://askubuntu.com/a/1163021/901082
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-10 g++-10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 120
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 120
+OPENSSL_ROOT_DIR=$CONDA_PREFIX pip download --no-binary :all: minerl==0.4.4
+# ERROR: Problem encountered: gfortran not found
+sudo apt install gfortran
+OPENSSL_ROOT_DIR=$CONDA_PREFIX pip download --no-binary :all: minerl==0.4.4
+#       ../scipy/meson.build:274:9: ERROR: Dependency "OpenBLAS" not found, tried pkgconfig and cmake
+sudo apt install libopenblas-dev
+OPENSSL_ROOT_DIR=$CONDA_PREFIX pip download --no-binary :all: minerl==0.4.4
+tar -xvf minerl-0.4.4.tar.gz
+cd minerl-0.4.4
+# remove gym line from requirements.txt
+pip install -r requirements.txt
+# Edit minerl-0.4.4/minerl/Malmo/Minecraft/build.gradle:L19 based on
+# https://github.com/MineDojo/MineDojo/issues/113#issuecomment-1908997704
+# classpath('MixinGradle-dcfaf61:MixinGradle:dcfaf61'){ // 0.6
+# Add repository maven to the build.gradle
+#         maven { url 'file:file:/absolute-path/to/that/repo's/parent' }
+pip install .
+
 python experiments/minerl044.py --image_width 64x64 --load simulation
 ```
 
-### MineRL 1.0.0 (minerl100)
+### MineRL 1.0.0 (exp_minerl100)
 ```bash
+conda create -n exp_minerl100 python=3.11
 conda activate exp_minerl100
+conda install conda-forge::openjdk=8
+pip install git+https://github.com/minerllabs/minerl
+pip install wandb tensorboard moviepy
 python experiments/minerl100.py --image_width 64x64 --load simulation
 ```
 
-### Craftground (craftground_raw | craftground_zerocopy)
+### Craftground (craftground_raw | craftground_zerocopy: exp_craftground)
 ```bash
+conda create -n exp_craftground python=3.11
 conda activate exp_craftground
-python experiments/craftground.py --mode=raw|zerocopy --image_width 64x64 --load simulation
+conda install conda-forge::openjdk=21
+pip install craftground
+pip install wandb tensorboard moviepy
+python experiments/craftground_exp.py --mode raw --image_width 64x64 --load simulation
 ```
--- experiments/craftground_exp.py
+++ experiments/craftground_exp.py
@@ -1,4 +1,117 @@
 import argparse
+import sys
+import time
+from craftground.screen_encoding_modes import ScreenEncodingMode
+from craftground.initial_environment_config import DaylightMode
+import wandb
+import gymnasium as gym
+import craftground
+from craftground import InitialEnvironmentConfig, ActionSpaceVersion
+from craftground.wrappers.vision import VisionWrapper
+from craftground.minecraft import no_op_v2
+
+from experiments import check_vglrun
+
+MAX_STEPS = 100_000
+
+
+def make_craftground_env(
+    port: int,
+    width: int,
+    height: int,
+    screen_encoding_mode: ScreenEncodingMode,
+    verbose_python: bool = False,
+    verbose_gradle: bool = True,
+    verbose_jvm: bool = True,
+) -> gym.Env:
+    return craftground.make(
+        port=port,
+        initial_env_config=InitialEnvironmentConfig(
+            image_width=width,
+            image_height=height,
+            hud_hidden=False,
+            render_distance=11,
+            screen_encoding_mode=screen_encoding_mode,
+        ).set_daylight_cycle_mode(DaylightMode.ALWAYS_DAY),
+        action_space_version=ActionSpaceVersion.V2_MINERL_HUMAN,
+        use_vglrun=check_vglrun(),
+        verbose_python=verbose_python,
+        verbose_gradle=verbose_gradle,
+        verbose_jvm=verbose_jvm,
+    )
+
+
+def simulation_check(
+    screen_encoding_mode: ScreenEncodingMode,
+    vision_width: int,
+    vision_height: int,
+    port: int,
+):
+    env = make_craftground_env(
+        port=port,
+        width=vision_width,
+        height=vision_height,
+        screen_encoding_mode=screen_encoding_mode,
+    )
+    VisionWrapper(env, x_dim=vision_width, y_dim=vision_height)
+    obs = env.reset()  # info
+    start_time = time.time_ns()
+    for i in range(MAX_STEPS):
+        action = no_op_v2()
+        obs, reward, terminated, info = env.step([action])  # truncated
+        time_elapsed = max((time.time_ns() - start_time) / 1e9, sys.float_info.epsilon)
+        fps = int(i / time_elapsed)
+        if i % 512 == 0:
+            wandb.log(
+                {
+                    "time/iterations": i,
+                    "time/fps": fps,
+                    "time/time_elapsed": int(time_elapsed),
+                    "time/total_timesteps": i,
+                }
+            )
+        if i % 4000 == 0:
+            print(f"Step: {i}")
+
+
+def do_experiment(mode, image_width, load, port):
+    screen_encoding_mode = {
+        "raw": ScreenEncodingMode.RAW,
+        "zerocopy": ScreenEncodingMode.ZEROCOPY,
+    }[mode]
+    vision_width, vision_height = {
+        "64x64": (64, 64),
+        "114x64": (114, 64),
+        "640x320": (640, 320),
+    }[image_width]
+
+    group_name = f"craftground-{mode}--{vision_width}-{vision_height}-{load}"
+    run = wandb.init(
+        # set the wandb project where this run will be logged
+        project="minecraft-envs-performance-comparison",
+        entity="jourhyang123",
+        group=group_name,
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        monitor_gym=True,  # auto-upload the videos of agents playing the game
+        save_code=True,  # optional
+    )
+
+    if load == "simulation":
+        simulation_check(screen_encoding_mode, vision_width, vision_height, port)
+    elif load == "render":
+        pass
+    elif load == "ppo":
+        pass
+    elif load == "render_ppo":
+        pass
+    elif load == "optimized_render":
+        pass
+    elif load == "optimized_ppo":
+        pass
+    elif load == "optimized_render_ppo":
+        pass
+    else:
+        raise ValueError(f"Unknown load configuration: {load}")
 
 
 def main():
@@ -38,12 +151,23 @@ def main():
         help="Specify the load configuration.",
     )
 
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8001,
+        help="Port number to use for the environment.",
+    )
+
     args = parser.parse_args()
 
     # Display the selected configuration
     print(f"Running experiment with the following settings:")
     print(f"Image Width: {args.image_width}")
     print(f"Load: {args.load}")
+    print(f"Mode: {args.mode}")
+    print(f"Port: {args.port}")
+
+    do_experiment(args.mode, args.image_width, args.load, args.port)
 
 
 if __name__ == "__main__":
