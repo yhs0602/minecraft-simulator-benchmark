510d38808c17e2dfe6e38eeb06b8f11d82b8b036
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-07 20:28:26 +0900

    :sparkles: Add malmo experimetns

--- run_malmo100_experiments.sh
+++ run_malmo100_experiments.sh
@@ -1,6 +1,13 @@
 vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
 vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
 
-vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
-vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
-vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
