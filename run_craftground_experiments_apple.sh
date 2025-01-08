3111be8dc7da4d0caed13fcaa73dec44614c0974
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-08 12:39:20 +0900

    🐛 Fix port

--- run_craftground_experiments_apple.sh
+++ run_craftground_experiments_apple.sh
@@ -1,16 +1,16 @@
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load simulation
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load simulation
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render_ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render_ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load simulation
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load simulation
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render_ppo
-python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render_ppo
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load simulation --port 8001
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load simulation --port 8002
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render --port 8003
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render --port 8004
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load ppo --port 8005
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load ppo --port 8006
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render_ppo --port 8007
+python experiments/craftground_exp.py --mode zerocopy --image_width 64x64 --load render_ppo --port 8008
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load simulation --port 8009
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load simulation --port 8010
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render --port 8011
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render --port 8012
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load ppo --port 8013
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load ppo --port 8014
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render_ppo --port 8015
+python experiments/craftground_exp.py --mode zerocopy --image_width 640x360 --load render_ppo --port 8016
