7200a664fe6f02f3f1e0163515dc55c5c3e6157a
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-20 15:44:51 +0900

    ✨ Add minerl100 scripts for sbx

---
+++ run_malmo100_experiments_sbx.sh
@@ -0,0 +1,23 @@
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo
+
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
+
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo --device cpu
+
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo --device cpu
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo --device cpu
