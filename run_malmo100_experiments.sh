ca224a761f1c249dec0ba2decfd626762d775487
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-27 20:55:55 +0900

    ⚗️ More seed

--- run_malmo100_experiments.sh
+++ run_malmo100_experiments.sh
@@ -1,13 +1,41 @@
 vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
 vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
-vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
-vglrun python experiments/minerl100_exp.py --image_width 64x64 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render
+
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
 vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
 vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_ppo
 
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 64x64 --load render_sbx-ppo
+
 vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
 vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
-vglrun python experiments/minerl100_exp.py --image_width 640x360 --load ppo
-vglrun python experiments/minerl100_exp.py --image_width 640x360 --load ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render
+
 vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
 vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_ppo
+
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
+vglrun python experiments/minerl100_exp.py --image_width 640x360 --load render_sbx-ppo
